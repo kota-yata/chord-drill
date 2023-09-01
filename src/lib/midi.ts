c9481ec73ef034c84a2cb1267aa2c83cc46d1c75
Kota Yatagai <kota.yatagai@plaid.co.jp>
2023-09-01 15:39:39 +0900

    add setTargetInput

--- src/lib/midi.ts
+++ src/lib/midi.ts
@@ -1,9 +1,13 @@
 export class MIDI {
   private midi: MIDIAccess;
+  private targetInput: MIDIInput | undefined;
   constructor(midiAccess: MIDIAccess) {
     this.midi = midiAccess;
   }
   public listInputs() {
     return this.midi.inputs;
+  }
+  public setTargetInput(input: MIDIInput) {
+    this.targetInput = input;
   }
 }
